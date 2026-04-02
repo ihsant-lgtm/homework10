64826e5fbf7b2b45ba9577e76ba951c2b5226aba
Ихсан <tairovihsan638@gmail.com>
2026-04-02 23:24:33 +0600

    feat: add types for basket and orders

--- src/types.ts
+++ src/types.ts
@@ -1,24 +1,24 @@
 export interface IDishShort {
-  name: string
-  description: string
-  price: number
+  name: string;
+  description: string;
+  price: number;
 }
 
 export interface IDishList {
-    [id:string]: IDishShort
+  [id: string]: IDishShort;
 }
 
-export interface IDish extends IDishShort{
-    id: string
+export interface IDish extends IDishShort {
+  id: string;
 }
 
 export interface IBasket {
-  dish: IDish
-  count: number
+  dish: IDish;
+  count: number;
 }
 
 export interface IBasketState {
-  items: IBasket[]
-  totalPrice: number
-  totalCount: number
+  items: IBasket[];
+  totalPrice: number;
+  totalCount: number;
 }
